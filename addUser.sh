3bd7e474bcad989c3ee5915699fc5646f133d616
kiteyoyo <kite821125@gmail.com>
2015-09-22 15:58:37 +0800

    update bug

--- addUser.sh
+++ addUser.sh
@@ -2,7 +2,6 @@
 if [ $1 ]; then
 	sudo adduser $1
 	sudo adduser $1 libvirtd
-	cp ~/kvm/
 	sudo cp ./management.py /home/$1/management.py
 	sudo mkdir /home/$1/kvm_log/
 	sudo chown $1 /home/$1/kvm_log
